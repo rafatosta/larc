09128d4787fb57888aa67da3c6b0a84fd16a3c98
Rafael Tosta <rafa.ecomp@gmail.com>
2025-10-21 17:35:25 -0300

    Add flowbite-react

--- src/renderer/src/pages/Login.tsx
+++ src/renderer/src/pages/Login.tsx
@@ -1,7 +1,18 @@
+import { Button } from "flowbite-react";
+import { Card } from "flowbite-react";
+
 function Login() {
     return (
         <div>
-            <p>Tela de login</p>
+            <Button>Click me</Button>
+            <Card href="#" className="max-w-sm">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Noteworthy technology acquisitions 2021
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                </p>
+            </Card>
         </div>);
 }
 
