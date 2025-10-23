be20911ed80ac38f3a2142a2e68fee09cd9bf2fe
Rafael Tosta <rafa.ecomp@gmail.com>
2025-10-23 08:54:47 -0300

    Create login form

--- src/renderer/src/pages/Login.tsx
+++ src/renderer/src/pages/Login.tsx
@@ -1,16 +1,40 @@
-import { Button } from "flowbite-react";
-import { Card } from "flowbite-react";
+import { Button, Checkbox, Label, TextInput, Card } from "flowbite-react";
 
 function Login() {
     return (
-        <div>
-            <Button>Click me</Button>
-            <Card href="#" className="max-w-sm">
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Noteworthy technology acquisitions 2021
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        <div className="flex min-h-svh w-full items-center justify-center">
+            <Card className="max-w-sm">
+                <form className="flex max-w-md flex-col gap-4">
+                    <div>
+                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Entre na sua conta
+                        </h5>
+                        <p className="text-sm font-normal text-gray-700 dark:text-gray-400">
+                            Digite seu email abaixo para acessar sua conta
+                        </p>
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="email">Email</Label>
+                        </div>
+                        <TextInput id="email" type="email" placeholder="nome@exemplo.com" required />
+                    </div>
+                    <div>
+                        <div className="mb-2 flex justify-between">
+                            <Label htmlFor="password">Senha</Label>
+                            <a className="text-sm font-normal text-gray-500 dark:text-gray-400 hover:underline" href="">Esqueceu sua senha?</a>
+                        </div>
+                        <TextInput id="password" type="password" required />
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Checkbox id="remember" />
+                        <Label htmlFor="remember">Lembre-me</Label>
+                    </div>
+                    <Button type="submit">Entrar</Button>
+                </form>
+
+                <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                    Não tem uma conta? <a href="" className="hover:underline">Cadastre-se</a>
                 </p>
             </Card>
         </div>);
